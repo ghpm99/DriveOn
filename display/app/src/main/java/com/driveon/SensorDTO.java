ee31cc668ed30b719be0d92bead00e5f508c06f2
Guilherme H <ghpm99@gmail.com>
2026-01-17 20:12:45 -0300

    feat: create sensor dto

---
+++ display/app/src/main/java/com/driveon/SensorDTO.java
@@ -0,0 +1,4 @@
+package com.driveon;
+
+public class SensorDTO {
+}
---
+++ display/app/src/main/java/com/driveon/SensorListener.java
@@ -0,0 +1,17 @@
+package com.driveon;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+
+public class SensorListener implements SensorEventListener {
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+}
